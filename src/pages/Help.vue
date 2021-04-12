41ee311b5330fce8280c081bd09cbabe6f57b413
Darren <theinvader360@gmail.com>
2021-04-12 15:01:05 +0100

    crud functionality (Todo.vue & quasar.conf.js)

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -1,6 +1,8 @@
 <template>
-  <q-page>
-    <h5>Help</h5>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam necessitatibus rem fugit laudantium cum. Est quisquam necessitatibus maiores rerum illo modi aspernatur delectus culpa cum, laudantium alias aperiam quidem illum!</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam necessitatibus rem fugit laudantium cum. Est quisquam necessitatibus maiores rerum illo modi aspernatur delectus culpa cum, laudantium alias aperiam quidem illum!</p>
   </q-page>
 </template>
 
